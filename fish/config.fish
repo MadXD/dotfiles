69ec947270348eabaa12218de686050060391e60
Lorenzo Villani <lorenzo@villani.me>
2014-05-27 08:00:42 +0200

    Test directories for presence before adding them to CDPATH, remove GIT_EDITOR

--- fish/config.fish
+++ fish/config.fish
@@ -6,12 +6,18 @@ set -e fish_greeting
 
 . ~/.config/fish/virtual.fish
 
-set -x CDPATH      ./ ~/Development/ ~/Development/develer/ ~/Dropbox ~/Dropbox/Config/ /usr/local/Library/Taps
+set -x CDPATH ./
+
+for cdentry in ~/Development/ ~/Development/develer/ ~/Dropbox ~/Dropbox/Config/ /usr/local/Library/Taps
+    if test -d $cdentry
+        set -x CDPATH $CDPATH $cdentry
+    end
+end
+
 set -x DEBEMAIL    lorenzo@villani.me
 set -x DEBFULLNAME Lorenzo Villani
 set -x EDITOR      'subl -n -w'
 set -x GEM_HOME    ~/.gem
-set -x GIT_EDITOR  nano
 set -x GOPATH      ~/.go
 set -x NPM_HOME    ~/.npm
 set -x PATH        $GEM_HOME/bin $GOPATH/bin $NPM_HOME/bin ~/.local/bin /usr/local/bin /bin /sbin /usr/bin /usr/sbin
