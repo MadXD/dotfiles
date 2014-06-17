c2c899c78af59d0f9080fe5982b0fbd882f4c2b3
Lorenzo Villani <lorenzo@villani.me>
2014-06-17 12:47:47 +0200

    fish: quote variables and separate sections

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,10 @@ set -e fish_greeting
 
 . ~/.config/fish/virtual.fish
 
+#
+# CDPATH
+#
+
 set -x CDPATH ./
 
 for cdentry in ~/Development/ ~/Development/develer/ ~/Dropbox ~/Dropbox/Config/ /usr/local/Library/Taps
@@ -14,8 +18,12 @@ for cdentry in ~/Development/ ~/Development/develer/ ~/Dropbox ~/Dropbox/Config/
     end
 end
 
-set -x DEBEMAIL    lorenzo@villani.me
-set -x DEBFULLNAME Lorenzo Villani
+#
+# Environment
+#
+
+set -x DEBEMAIL    'lorenzo@villani.me'
+set -x DEBFULLNAME 'Lorenzo Villani'
 set -x EDITOR      'subl -n -w'
 set -x GEM_HOME    ~/.gem
 set -x GIT_EDITOR  nano
@@ -23,8 +31,11 @@ set -x GOPATH      ~/.go
 set -x NPM_HOME    ~/.npm
 set -x PATH        $GEM_HOME/bin $GOPATH/bin $NPM_HOME/bin ~/.local/bin /usr/local/bin /bin /sbin /usr/bin /usr/sbin
 
-alias e="$EDITOR"
-alias r=". ~/.config/fish/config.fish"
+#
+# Aliases
+#
 
 alias blog="$EDITOR ~/Dropbox/lvillani.github.io/"
 alias dotfiles="$EDITOR ~/Dropbox/Config/dotfiles"
+alias e="$EDITOR"
+alias r=". ~/.config/fish/config.fish"
