ef837bb5136ac51ec73b04a493c2057c5b0be60b
Lorenzo Villani <lorenzo@villani.me>
2014-07-22 14:27:08 +0200

    Update fish configuration

--- fish/config.fish
+++ fish/config.fish
@@ -29,7 +29,15 @@ set -x GEM_HOME    ~/.gem
 set -x GIT_EDITOR  nano
 set -x GOPATH      ~/.go
 set -x NPM_HOME    ~/.npm
-set -x PATH        $GEM_HOME/bin $GOPATH/bin $NPM_HOME/bin ~/.local/bin /usr/local/bin /bin /sbin /usr/bin /usr/sbin
+set -x VAGRANT_DEFAULT_PROVIDER vmware_fusion
+set -x VAGRANT_VMWARE_CLONE_DIRECTORY ~/.vagrant.d/vmware
+set -x VAGRANT_VMWARE_FUSION_APP ~/Applications/VMware\ Fusion.app
+
+# Override PATH only if we are not inside a Python Virtualenv, in which case we want to pick the
+# python executable from its path.
+if test -z "$VIRTUAL_ENV" -a -z "$COMELZ_QT_SDK_SETUP_DONE"
+    set -x PATH $GEM_HOME/bin $GOPATH/bin $NPM_HOME/bin ~/.local/bin /usr/local/bin /bin /sbin /usr/bin /usr/sbin
+end
 
 #
 # Aliases
