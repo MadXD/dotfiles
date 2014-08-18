91b13a7f60f25eec451ff616ba46b995d89d30d6
Lorenzo Villani <lorenzo@villani.me>
2014-08-18 20:02:44 +0200

    Test only VIRTUALENV variable

--- fish/config.fish
+++ fish/config.fish
@@ -37,8 +37,8 @@ set -x VAGRANT_VMWARE_FUSION_APP ~/Applications/VMware\ Fusion.app
 set -x npm_config_prefix $NPM_HOME
 
 # Override PATH only if we are not inside a Python Virtualenv, in which case we want to pick the
-# python executable from its path.
-if test -z "$VIRTUAL_ENV" -a -z "$COMELZ_QT_SDK_SETUP_DONE"
+# python executable from its PATH.
+if test -z "$VIRTUAL_ENV"
     set -x PATH $GEM_HOME/bin $GOPATH/bin $NPM_HOME/bin ~/.local/bin /usr/local/bin /bin /sbin /usr/bin /usr/sbin
 end
 
