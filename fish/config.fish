600db0fa3c49e9f38c730454edd0d9a67b891ca0
Lorenzo Villani <lorenzo@villani.me>
2014-09-03 15:41:42 +0200

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -11,11 +11,11 @@ source ~/.config/fish/virtualfish/auto_activation.fish
 # CDPATH
 #
 
-set -x CDPATH ./
+set -U CDPATH . "$HOME"
 
 for cdentry in ~/Development/ ~/Development/develer/
     if test -d $cdentry
-        set -x CDPATH $CDPATH $cdentry
+        set -U CDPATH $CDPATH $cdentry
     end
 end
 
@@ -23,21 +23,22 @@ end
 # Environment
 #
 
-set -x DEBEMAIL    lorenzo@villani.me
+set -e LC_CTYPE  # Work-around OS X bug
+
+set -x DEBEMAIL    "lorenzo@villani.me"
 set -x DEBFULLNAME "Lorenzo Villani"
-set -x EDITOR "emacsclient -c -a ''"
+set -x EDITOR      "emacsclient -c -a ''"
 set -x GEM_HOME    ~/.gem
 set -x GOPATH      ~/.go
 set -x NPM_HOME    ~/.npm
 set -x VAGRANT_DEFAULT_PROVIDER vmware_fusion
 set -x VAGRANT_VMWARE_CLONE_DIRECTORY ~/.vagrant.d/vmware
 set -x VAGRANT_VMWARE_FUSION_APP ~/Applications/VMware\ Fusion.app
-
 set -x npm_config_prefix $NPM_HOME
 
-# Override PATH only if we are not inside a Python Virtualenv, in which case we want to pick the
-# python executable from its PATH.
-if test -z "$VIRTUAL_ENV"
+if test -n "$VIRTUAL_ENV"
+    set -x PATH $VIRTUAL_ENV/bin /usr/local/bin $PATH
+else
     set -x PATH $GEM_HOME/bin $GOPATH/bin $NPM_HOME/bin ~/.local/bin /usr/local/bin /bin /sbin /usr/bin /usr/sbin
 end
 
@@ -46,10 +47,7 @@ end
 #
 
 alias e="$EDITOR"
-alias mai="make ; and make install"
+alias g="git"
+alias m="make; and make install"
 alias r="source ~/.config/fish/config.fish"
-alias search="find . -type f -iname"
-
-if test -x /usr/local/bin/hub -o -x /usr/bin/hub
-    alias git="hub"
-end
+alias s="find . -type f -iname"
