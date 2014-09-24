9c89b33bc3d660ef7a0f7c0f505ff602063f895e
Lorenzo Villani <lvillani@develer.com>
2014-09-24 16:58:06 +0200

    Moar environment variables

--- fish/config.fish
+++ fish/config.fish
@@ -27,14 +27,15 @@ set -e LC_CTYPE  # Work-around OS X bug
 
 set -x DEBEMAIL    "lorenzo@villani.me"
 set -x DEBFULLNAME "Lorenzo Villani"
-set -x EDITOR      "emacsclient -c -a ''"
+set -x EDITOR      subl
 set -x GEM_HOME    ~/.gem
+set -x GIT_EDITOR  nano
 set -x GOPATH      ~/.go
+set -x npm_config_prefix $NPM_HOME
 set -x NPM_HOME    ~/.npm
 set -x VAGRANT_DEFAULT_PROVIDER vmware_fusion
 set -x VAGRANT_VMWARE_CLONE_DIRECTORY ~/.vagrant.d/vmware
 set -x VAGRANT_VMWARE_FUSION_APP ~/Applications/VMware\ Fusion.app
-set -x npm_config_prefix $NPM_HOME
 
 if test -n "$VIRTUAL_ENV"
     set -x PATH $VIRTUAL_ENV/bin /usr/local/bin $PATH
