605bae1422ae154377db4e5bc769ef5853eccad5
Lorenzo Villani <lorenzo@villani.me>
2014-12-28 15:07:50 +0100

    fish: simplify config

--- fish/config.fish
+++ fish/config.fish
@@ -23,25 +23,20 @@ end
 # Environment
 #
 
-set -x DEBEMAIL    "lorenzo@villani.me"
+set -x DEBEMAIL "lorenzo@villani.me"
 set -x DEBFULLNAME "Lorenzo Villani"
-set -x EDITOR      "subl"
-set -x GEM_HOME    "$HOME/.gem"
-set -x GIT_EDITOR  "nano"
-set -x GOPATH      "$HOME/.go"
+set -x EDITOR "subl"
+set -x GEM_HOME "$HOME/.gem"
+set -x GIT_EDITOR "nano"
+set -x GOPATH "$HOME/Development/Go"
 set -x LPASS_AGENT_TIMEOUT 28800  # 8 hours
-set -x NPM_HOME    "$HOME/.npm"
-set -x VAGRANT_DEFAULT_PROVIDER       "vmware_fusion"
+set -x NPM_HOME "$HOME/.npm"
+set -x VAGRANT_DEFAULT_PROVIDER "vmware_fusion"
 set -x VAGRANT_VMWARE_CLONE_DIRECTORY "$HOME/.vagrant.d/vmware"
-set -x VAGRANT_VMWARE_FUSION_APP      "$HOME/Applications/VMware Fusion.app"
+set -x VAGRANT_VMWARE_FUSION_APP "$HOME/Applications/VMware Fusion.app"
 
 set -x npm_config_prefix "$NPM_HOME"
-
-if test -n "$VIRTUAL_ENV"
-    set -x PATH "$VIRTUAL_ENV/bin" "/usr/local/bin" "$PATH"
-else
-    set -x PATH "$GEM_HOME/bin" "$GOPATH/bin" "$NPM_HOME/bin" "$HOME/.local/bin" "/usr/local/bin" "/usr/local/sbin" "/usr/bin" "/usr/sbin" "/bin" "/sbin"
-end
+set -x PATH "$GEM_HOME/bin" "$GOPATH/bin" "$NPM_HOME/bin" "$HOME/.local/bin" "/usr/local/bin" "/usr/local/sbin" "/usr/bin" "/usr/sbin" "/bin" "/sbin"
 
 #
 # Aliases
