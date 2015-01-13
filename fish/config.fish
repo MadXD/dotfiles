2f37f7378c529afb006c5a8c7284932ceed6eea2
Lorenzo Villani <lorenzo@villani.me>
2015-01-13 15:38:15 +0100

    More fish settings

--- fish/config.fish
+++ fish/config.fish
@@ -31,12 +31,16 @@ set -x GIT_EDITOR "nano"
 set -x GOPATH "$HOME/Development/Go"
 set -x LPASS_AGENT_TIMEOUT 28800  # 8 hours
 set -x NPM_HOME "$HOME/.npm"
+set -x PIP_ALLOW_ALL_EXTERNAL 1
+set -x PIP_USER 1
 set -x VAGRANT_DEFAULT_PROVIDER "vmware_fusion"
 set -x VAGRANT_VMWARE_CLONE_DIRECTORY "$HOME/.vagrant.d/vmware"
 set -x VAGRANT_VMWARE_FUSION_APP "$HOME/Applications/VMware Fusion.app"
 
-set -x npm_config_prefix "$NPM_HOME"
-set -x PATH "$GEM_HOME/bin" "$GOPATH/bin" "$NPM_HOME/bin" "$HOME/.local/bin" "/usr/local/bin" "/usr/local/sbin" "/usr/bin" "/usr/sbin" "/bin" "/sbin"
+if test -z $VIRTUAL_ENV
+    set -x npm_config_prefix "$NPM_HOME"
+    set -x PATH "$GEM_HOME/bin" "$GOPATH/bin" "$NPM_HOME/bin" "$HOME/Library/Python/2.7/bin" "$HOME/.local/bin" "/usr/local/bin" "/usr/local/sbin" "/usr/bin" "/usr/sbin" "/bin" "/sbin"
+end
 
 #
 # Aliases
