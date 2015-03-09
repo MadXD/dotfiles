05ad5cc982bb8df3588325743a6cc2b1310272c4
Lorenzo Villani <lorenzo@villani.me>
2015-03-09 20:35:07 +0100

    Remove emacsclient

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -2,13 +2,13 @@
 # Environment
 #
 
-$env:EDITOR = "emacsclient -c -a ''"
 $env:GEM_HOME = "$env:UserProfile\.gem"
 $env:GIT_SSH = "$env:SystemDrive\Program Files (x86)\PuTTY\plink.exe"
 $env:GOPATH = "$env:UserProfile\Development\Go"
 $env:NPM_HOME = "$env:UserProfile\.npm"
 $env:PATH = "$env:SystemRoot\System32\WindowsPowerShell\v1.0;$env:SystemRoot\system32;$env:SystemRoot;$env:SystemRoot\System32\Wbem"
 $env:SVN_SSH = "$env:SystemDrive\\Program Files (x86)\\PuTTY\\plink.exe"
+
 $env:npm_config_prefix = "$env:NPM_HOME"
 
 $env:PATH = "$env:GEM_HOME\bin;$env:GOPATH\bin;$env:NPM_HOME\bin;$env:SystemDrive\just-install;$env:PATH"
